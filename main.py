bc181c7f6397bf204b9b92ea8c76bb22b19f1640
justatest420 <justatestsubject.01@gmail.com>
2025-05-16 11:40:54 +0545

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def serve_html():
-    return send_from_directory('static', 'your_page.html')
+    return send_from_directory('HTML', 'nepal.html') # /.html
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
